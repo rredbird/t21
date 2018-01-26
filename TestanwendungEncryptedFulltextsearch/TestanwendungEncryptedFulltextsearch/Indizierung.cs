01be564c8721b5c3bd035c4110aca2fc3e97e0bd
Robin Ruth <rredbird@gmail.com>
2018-01-26 19:25:20 +0000

    Rudimentary support for more than one keyword.

--- TestanwendungEncryptedFulltextsearch/TestanwendungEncryptedFulltextsearch/Indizierung.cs
+++ TestanwendungEncryptedFulltextsearch/TestanwendungEncryptedFulltextsearch/Indizierung.cs
@@ -12,16 +12,22 @@ namespace TestanwendungEncryptedFulltextsearch
         public void IndexDocuments()
         {
             var documents = LoadDocuments();
+
+            foreach (var document in documents)
+            {
+                IndexDocument(document.Item2, document.Item1);
+            }
         }
 
-        private object LoadDocuments()
+        private List<Tuple<string, string>> LoadDocuments()
         {
+            var retVal = new List<Tuple<string, string>>();
             foreach (var path in Directory.GetFiles(documentPath, "*.txt"))
             {
                 var document = File.OpenText(path).ReadToEnd();
-
-                IndexDocument(document, Path.GetFileName(document));
+                retVal.Add(Tuple.Create<string, string>(Path.GetFileName(path), document));
             }
+            return retVal;
         }
 
         private Dictionary<string, List<Suchtreffer>> Index = new Dictionary<string, List<Suchtreffer>>();
@@ -30,8 +36,9 @@ namespace TestanwendungEncryptedFulltextsearch
             var words = document.Split(' ', '.', ',', '!', '?', ':', ';');
             var counter = 0;
 
-            foreach (var word in words)
+            foreach (var w in words)
             {
+                var word = w.ToLower();
                 if (Index.ContainsKey(word))
                 {
                     Index[word].Add(new Suchtreffer(counter, filename));
@@ -42,6 +49,28 @@ namespace TestanwendungEncryptedFulltextsearch
                 }
                 counter++;
             }
+        }
+
+        public List<Suchtreffer> Search(string query)
+        {
+            List<List<Suchtreffer>> indicees = new List<List<Suchtreffer>>();
+            var words = query.ToLower().Split(' ');
+            foreach (var word in words)
+            {
+                if (Index.ContainsKey(word))
+                {
+                    indicees.Add(Index[word]);
+                }
+            }
+            var retVal = new List<Suchtreffer>();
+            if (indicees.Count == words.Length)
+            {
+                foreach (var index in indicees)
+                {
+                    retVal.AddRange(index);
+                }
+            }
+            return retVal;
         }
     }
 }
--- TestanwendungEncryptedFulltextsearch/TestanwendungEncryptedFulltextsearch/Program.cs
+++ TestanwendungEncryptedFulltextsearch/TestanwendungEncryptedFulltextsearch/Program.cs
@@ -7,6 +7,32 @@ namespace TestanwendungEncryptedFulltextsearch
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var indizierung = new Indizierung();
+            indizierung.IndexDocuments();
+
+            Console.WriteLine("Bond Wikipedia");
+            foreach (var searchresult in indizierung.Search("Bond Wikipedia"))
+            {
+                Console.WriteLine(searchresult.filename + ":" + searchresult.wordnumber);
+            }
+            Console.WriteLine("Wikipedia ship");
+            foreach (var searchresult in indizierung.Search("Wikipedia ship"))
+            {
+                Console.WriteLine(searchresult.filename + ":" + searchresult.wordnumber);
+            }
+            Console.WriteLine("DIN Norm");
+            foreach (var searchresult in indizierung.Search("DIN Norm"))
+            {
+                Console.WriteLine(searchresult.filename + ":" + searchresult.wordnumber);
+            }
+            Console.WriteLine("ship");
+            foreach (var searchresult in indizierung.Search("ship"))
+            {
+                Console.WriteLine(searchresult.filename + ":" + searchresult.wordnumber);
+            }
+
+            Console.ReadKey();
         }
     }
 }
